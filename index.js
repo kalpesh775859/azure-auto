1c74fde5016159fe332f432ece7b159e35518443
Kalpesh Ambuskar <kalpeshambuskar@gmail.com>
2026-04-28 21:19:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Auto Deploy Working 🚀");
+    res.send("CI/CD Auto Deploy Working 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server Running...");
+    console.log("Server Running...");
 });
